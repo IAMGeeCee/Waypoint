03963841a0b9ff2028c6e987dae1264a8daa5a75
George Clensy <117567086+IAMGeeCee@users.noreply.github.com>
2023-10-18 16:26:17 +0000

    Added map and coordinates box which returns json for navigation

--- Waypoint/Controllers/AppController.cs
+++ Waypoint/Controllers/AppController.cs
@@ -1,11 +1,42 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Waypoint.Controllers;
-
-public class AppController : Controller
+namespace Waypoint.Controllers
 {
-   public IActionResult Map(){
-    return View();
+
+   public class AppController : Controller
+   {
+      public IActionResult Map()
+      {
+         return View();
+      }
+
+      public IActionResult ShowDirections(string json)
+      {
+         return Content(json);
+      }
+
+      public async Task<IActionResult> CalculateDirections(string coordinates)
+      {
+         string apiKey = "88ed98eb-c3dc-4b84-be54-f3862db93f24";
+         string apiUrl = $"https://graphhopper.com/api/1/route?point=51.5074,-0.1278&point={coordinates}&vehicle=car&locale=en&key={apiKey}";
+
+         using (HttpClient client = new HttpClient())
+         {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+               string json = await response.Content.ReadAsStringAsync();
+               return Content(json);
+            }
+            else
+            {
+               throw new Exception($"GraphHopper API request failed with status code {response.StatusCode}");
+            }
+         }
+      }
+
+
    }
 }
